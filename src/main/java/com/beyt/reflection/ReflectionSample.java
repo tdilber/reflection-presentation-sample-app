095e9f2675376e174369a3e5e0a9347b18c72712
talhadilber <talhadilber@hipicon.com>
2022-03-15 23:11:28 +0300

    ReflectionFull Class Sample annotations Added

--- src/main/java/com/beyt/reflection/ReflectionSample.java
+++ src/main/java/com/beyt/reflection/ReflectionSample.java
@@ -7,11 +7,9 @@ import com.beyt.reflection.dto.UserDTO;
 import com.beyt.reflection.service.UserService;
 import com.beyt.reflection.util.GenericTypeResolverUtil;
 import org.apache.commons.lang3.ArrayUtils;
-import org.springframework.aop.framework.AopProxyUtils;
 
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -39,8 +37,6 @@ public class ReflectionSample {
 
        // if(true) return;
 
-
-
         System.out.println("---------------------");
         printClass(UserController.class);
         System.out.println("---------------------");
@@ -53,20 +49,27 @@ public class ReflectionSample {
         StringBuilder sb = new StringBuilder();
 
         sb.append("package ").append(clazz.getPackage().getName()).append(";\n\n\n")
+                .append(Arrays.stream(clazz.getAnnotations()).map(a -> "@" + a.annotationType().getSimpleName() + "\n").collect(Collectors.joining()))
                 .append(Modifier.toString(clazz.getModifiers())).append(" class ").append(clazz.getSimpleName())
                 .append(" {\n\n");
 
         Stream.of(clazz.getDeclaredFields()).forEach(field ->
-                sb.append("\t").append(Modifier.toString(field.getModifiers())).append(" ").append(GenericTypeResolverUtil.resolveFieldGenericsTree(field))
+                sb.append("\t").append(Arrays.stream(field.getAnnotations()).map(a -> "@" + a.annotationType().getSimpleName() + "\n").collect(Collectors.joining()))
+                        .append("\t").append(Modifier.toString(field.getModifiers())).append(" ").append(GenericTypeResolverUtil.resolveFieldGenericsTree(field).printResult())
                         .append(" ").append(field.getName()).append(";\n\n")
         );
 
-        Stream.of(clazz.getDeclaredConstructors()).forEach(constructor -> sb.append("\t").append(Modifier.toString(constructor.getModifiers()))
-                .append(" ").append(clazz.getSimpleName()).append("(")
-                .append(Stream.of(constructor.getParameters()).map(p -> GenericTypeResolverUtil.resolveConstructorParameterGenericsTree(constructor, ArrayUtils.indexOf(constructor.getParameters(), p)).printResult() + " " + p.getName()).collect(Collectors.joining(", ")))
-                .append(") { }\n\n"));
+        Stream.of(clazz.getDeclaredConstructors()).forEach(constructor ->
+                sb
+                        .append("\t").append(Arrays.stream(constructor.getAnnotations()).map(a -> "@" + a.annotationType().getSimpleName() + "\n").collect(Collectors.joining()))
+                        .append("\t").append(Modifier.toString(constructor.getModifiers()))
+                        .append(" ").append(clazz.getSimpleName()).append("(")
+                        .append(Stream.of(constructor.getParameters()).map(p -> GenericTypeResolverUtil.resolveConstructorParameterGenericsTree(constructor, ArrayUtils.indexOf(constructor.getParameters(), p)).printResult() + " " + p.getName()).collect(Collectors.joining(", ")))
+                        .append(") { }\n\n"));
 
-        Stream.of(clazz.getDeclaredMethods()).forEach(method -> sb.append("\t").append(Modifier.toString(method.getModifiers()))
+        Stream.of(clazz.getDeclaredMethods()).forEach(method -> sb
+                .append("\t").append(Arrays.stream(method.getAnnotations()).map(a -> "@" + a.annotationType().getSimpleName() + "\n").collect(Collectors.joining()))
+                .append("\t").append(Modifier.toString(method.getModifiers()))
                 .append(" ").append(GenericTypeResolverUtil.resolveReturnTypeGenericsTree(method).printResult()).append(" ").append(method.getName()).append("(")
                 .append(Stream.of(method.getParameters()).map(p -> GenericTypeResolverUtil.resolveMethodParameterGenericsTree(method, ArrayUtils.indexOf(method.getParameters(), p)).printResult() + " " + p.getName()).collect(Collectors.joining(", ")))
                 .append(") { }\n\n"));
