23eb92f13c9698b821649c1f6db309cfe23db45b
talhadilber <talhadilber@hipicon.com>
2022-06-08 23:00:46 +0300

    Metrics Test added

--- src/main/java/com/beyt/reflection/ReflectionMetricsTest.java
+++ src/main/java/com/beyt/reflection/ReflectionMetricsTest.java
@@ -5,10 +5,12 @@ import com.beyt.reflection.service.MetricsTestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Profile("metrics")
 public class ReflectionMetricsTest implements ApplicationRunner {
 
     private final MetricsTestService metricsTestService;
@@ -19,7 +21,7 @@ public class ReflectionMetricsTest implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        //timedTest();
+        timedTest();
     }
 
     protected void timedTest() {
@@ -27,6 +29,7 @@ public class ReflectionMetricsTest implements ApplicationRunner {
         metricsTestService.empty();
         metricsTestService.empty();
         log.info("---------------------------------");
+        log.info("-------------- Load Test -------------------");
         metricsTestService.normallyRun();
         metricsTestService.normallyRun();
         metricsTestService.normallyRun();
@@ -42,6 +45,20 @@ public class ReflectionMetricsTest implements ApplicationRunner {
         metricsTestService.reflectionFindAllTime();
         metricsTestService.reflectionFindAllTime();
         metricsTestService.reflectionFindAllTime();
+        log.info("---------------------------------");
+        log.info("-------------- Free Runners -------------------");
+        metricsTestService.freeRunNormal();
+        log.info("---------------------------------");
+        metricsTestService.freeRunReflectionOnlyCall();
+        log.info("---------------------------------");
+        metricsTestService.freeRunReflectionFindOnce();
+        log.info("---------------------------------");
+        metricsTestService.freeRunReflectionFindAllTime();
+        log.info("---------------------------------");
+        log.info("----------------Instance Create -----------------");
+        metricsTestService.instanceCreateNormal();
+        log.info("---------------------------------");
+        metricsTestService.instanceCreateReflection();
         log.info("--------------- Metrics End ------------------");
     }
 }
--- src/main/java/com/beyt/reflection/service/MetricsTestService.java
+++ src/main/java/com/beyt/reflection/service/MetricsTestService.java
@@ -1,22 +1,27 @@
 package com.beyt.reflection.service;
 
 import com.beyt.reflection.aspect.LogTime;
-import io.micrometer.core.annotation.Timed;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.regex.Matcher;
+import java.text.DecimalFormat;
 
+@Slf4j
 @Service
 public class MetricsTestService {
 
-    public static final int LOOP_COUNT = 1000000;
+    public static final int LOOP_COUNT = 100;
 
-    private Method method;
+    private final Method method;
+    private final Method methodFreeRunner;
+    private final DecimalFormat formatter;
 
     public MetricsTestService() throws NoSuchMethodException {
         method = MetricsTestService.class.getDeclaredMethod("randomCalculation");
+        methodFreeRunner = MetricsTestService.class.getDeclaredMethod("freeRunner", Long.class);
+        formatter = new DecimalFormat("#,###");
     }
 
     @LogTime
@@ -65,6 +70,69 @@ public class MetricsTestService {
         }
     }
 
+
+    public void freeRunNormal() {
+        Long time = System.currentTimeMillis();
+        Long counter = 0L;
+        while (System.currentTimeMillis() - time < 1000) {
+            counter = freeRunner(counter);
+        }
+        log.info("Free Runner Normal Count : {}", formatter.format(counter));
+    }
+
+    @SneakyThrows
+    public void freeRunReflectionOnlyCall() {
+        Long time = System.currentTimeMillis();
+        Long counter = 0L;
+        while (System.currentTimeMillis() - time < 1000) {
+            counter = (Long) methodFreeRunner.invoke(this, counter);
+        }
+        log.info("Free Runner ReflectionOnlyCall Count : {}", formatter.format(counter));
+    }
+
+    @SneakyThrows
+    public void freeRunReflectionFindOnce() {
+        Long time = System.currentTimeMillis();
+        Long counter = 0L;
+        Method methodFreeRunner = MetricsTestService.class.getDeclaredMethod("freeRunner", Long.class);
+        while (System.currentTimeMillis() - time < 1000) {
+            counter = (Long) methodFreeRunner.invoke(this, counter);
+        }
+        log.info("Free Runner ReflectionFindOnce Count : {}", formatter.format(counter));
+    }
+
+    @SneakyThrows
+    public void freeRunReflectionFindAllTime() {
+        Long time = System.currentTimeMillis();
+        Long counter = 0L;
+        while (System.currentTimeMillis() - time < 1000) {
+            counter = (Long) MetricsTestService.class.getDeclaredMethod("freeRunner", Long.class).invoke(this, counter);
+        }
+        log.info("Free Runner ReflectionFindAllTime Count : {}", formatter.format(counter));
+    }
+
+    @SneakyThrows
+    public void instanceCreateNormal() {
+        Long time = System.currentTimeMillis();
+        Long counter = 0L;
+        while (System.currentTimeMillis() - time < 1000) {
+            MetricsTestService metricsTestService = new MetricsTestService();
+            counter++;
+        }
+        log.info("Instance Create Normal Count : {}", formatter.format(counter));
+    }
+
+    @SneakyThrows
+    public void instanceCreateReflection() {
+        Long time = System.currentTimeMillis();
+        Long counter = 0L;
+        while (System.currentTimeMillis() - time < 1000) {
+            MetricsTestService metricsTestService = MetricsTestService.class.getConstructor().newInstance();
+            counter++;
+        }
+        log.info("Instance Create Reflection Count : {}", formatter.format(counter));
+    }
+
     private void randomCalculation() {
         double result = 1;
         for (int i = 0; i < 150; i++) {
@@ -74,4 +142,7 @@ public class MetricsTestService {
         }
     }
 
+    private Long freeRunner(Long count) {
+        return count + 1;
+    }
 }
